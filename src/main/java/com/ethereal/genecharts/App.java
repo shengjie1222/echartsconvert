f3872e804e4ff4ae1b0a8ec9a191d70cc125a7b4
shengjie <1370478022@qq.com>
2021-06-09 14:30:31 +0800

    Initial commit

---
+++ src/main/java/com/ethereal/genecharts/App.java
@@ -0,0 +1,58 @@
+package com.ethereal.genecharts;
+
+/**
+ * @author Administrator
+ * @Description
+ * @create 2021-06-09 13:23
+ */
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashMap;
+
+import com.ethereal.genecharts.util.EchartsUtil;
+import com.ethereal.genecharts.util.FreemarkerUtil;
+
+import com.alibaba.fastjson.JSON;
+
+import freemarker.template.TemplateException;
+import sun.misc.BASE64Decoder;
+
+public class App {
+    public static void main(String[] args) throws IOException, TemplateException {
+        // 变量
+        String title = "水果";
+        String[] categories = new String[] { "苹果", "香蕉", "西瓜" };
+        int[] values = new int[] { 3, 2, 1 };
+
+        // 模板参数
+        HashMap<String, Object> datas = new HashMap<>();
+        datas.put("categories", JSON.toJSONString(categories));
+        datas.put("values", JSON.toJSONString(values));
+        datas.put("title", title);
+
+        // 生成option字符串
+        String option = FreemarkerUtil.generateString("option.ftl", "", datas);
+
+        // 根据option参数
+        String base64 = EchartsUtil.generateEchartsBase64(option);
+
+        System.out.println("BASE64:" + base64);
+        generateImage(base64, "C:/Users/Administrator/Desktop/test.png");
+    }
+
+    public static void generateImage(String base64, String path) throws IOException {
+        BASE64Decoder decoder = new BASE64Decoder();
+        try (OutputStream out = new FileOutputStream(path)){
+            // 解密
+            byte[] b = decoder.decodeBuffer(base64);
+            for (int i = 0; i < b.length; ++i) {
+                if (b[i] < 0) {
+                    b[i] += 256;
+                }
+            }
+            out.write(b);
+            out.flush();
+        }
+    }
+}
---
+++ src/main/java/com/ethereal/genecharts/util/FreemarkerUtil.java
@@ -0,0 +1,41 @@
+package com.ethereal.genecharts.util;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.Map;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+/**
+ * @author Administrator
+ * @Description
+ * @create 2021-06-09 13:22
+ */
+
+public class FreemarkerUtil {
+    private static final String path = FreemarkerUtil.class.getClassLoader().getResource("template").getPath();
+
+    public static String generateString(String templateFileName, String templateDirectory, Map<String, Object> datas)
+            throws IOException, TemplateException {
+        Configuration configuration = new Configuration(Configuration.VERSION_2_3_0);
+
+        // 设置默认编码
+        configuration.setDefaultEncoding("UTF-8");
+
+        // 设置模板所在文件夹
+        configuration.setDirectoryForTemplateLoading(new File(path + templateDirectory));
+
+        // 生成模板对象
+        Template template = configuration.getTemplate(templateFileName);
+
+        // 将datas写入模板并返回
+        try (StringWriter stringWriter = new StringWriter()) {
+            template.process(datas, stringWriter);
+            stringWriter.flush();
+            return stringWriter.getBuffer().toString();
+        }
+    }
+}
